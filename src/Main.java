75c9db4cb4834b0f86b4431539a43328e08bf37e
Manex <maranzadieg23wg@ikzubirimanteo.com>
2023-11-14 13:36:12 +0100

    Menua gehituta

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Aukeratu eragiketa bat:");
+        System.out.println("1.Batuketa       2.Kenketa");
+        System.out.println("3.Biderketa      4.Zatiketa");
+        System.out.println("5.Berreketa      6.Hondarra");
+        int eragiketa = sc.nextInt();
 
-        //proba
 
-        //proba 2
 
-        //proba3
     }
 }
